d848a6a10a985ebe78ef9e8fb5b39e5503e038b4
Quzy <40630448+QuzyTheFirst@users.noreply.github.com>
2024-03-02 16:19:33 +0000

    Completed UI

--- js/main.js
+++ js/main.js
@@ -4,69 +4,87 @@ const paper = "paper";
 const scissors = "scissors";
 const attackTypes = [rock, paper, scissors];
 
+// Get UI Buttons
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+
+// Get UI
+const choices = document.querySelector(".choices");
+const playerPoints = document.querySelector(".player-points");
+const computerPoints = document.querySelector(".computer-points");
+const playerChoice = document.querySelector(".player-choice");
+const computerChoice = document.querySelector(".computer-choice");
+const roundWinner = document.querySelector(".round-winner");
+const gameWinner = document.querySelector(".game-winner");
+
 // Points variable
 let computerWinPoints = 0;
 let playerWinPoints = 0;
 
-// Start Game
-PlayGame(5);
+// Initialize game buttons
+rockButton.addEventListener('click', () => {
+     PlayRound(rock, GetComputerChoice());
+});
 
+paperButton.addEventListener('click', () => {
+    PlayRound(paper, GetComputerChoice());
+});
 
-function PlayGame(roundsCount) {
-    // Clear points
-    computerWinPoints = 0;
-    playerWinPoints = 0;
+scissorsButton.addEventListener('click', () =>{
+    PlayRound(scissors, GetComputerChoice());
+});
 
-    for (let i = 0; i < roundsCount; i++) {
-        // Variables for player and computer selected attacks
-        let computerSelection = "";
-        let playerSelection = "";
-
-        // Let pc choose his attack type
-        computerSelection = GetComputerChoice();
-        console.log("Computer: " + computerSelection);
-
-        // Ask user for his attack type
-        do {
-            playerSelection = prompt("Choose your attack type[rock, paper, scissors]:")?.toLowerCase();
-        } while (!IsAttackValid(playerSelection))
-        console.log("Player: " + playerSelection);
-
-        // Check who has won a battle
-        PlayRound(playerSelection, computerSelection);
-    }
-
-    FindWinner();
-}
 
 function PlayRound(playerSelection, computerSelection) {
+    if(CheckForWinner())
+        return;
+
     // your code here!
     if (playerSelection === computerSelection) {
-        console.log(" | This round is a tie!");
+        playerChoice.textContent = playerSelection;
+        computerChoice.textContent = computerSelection;
+        roundWinner.textContent = "This round is a tie!";
     }
     else if (
         playerSelection === paper && computerSelection === rock ||
         playerSelection === rock && computerSelection === scissors ||
         playerSelection === scissors && computerSelection === paper
     ) {
-        console.log(" | Player has won a round!");
+        playerChoice.textContent = playerSelection;
+        computerChoice.textContent = computerSelection;
+        roundWinner.textContent = "Player has won a round!";
         playerWinPoints++;
+        playerPoints.textContent = `Player win points: ${playerWinPoints}`
     }
     else {
-        console.log(" | Computer has won a round!");
+        playerChoice.textContent = playerSelection;
+        computerChoice.textContent = computerSelection;
+        roundWinner.textContent = "Computer has won a round!";
         computerWinPoints++;
+        computerPoints.textContent = `Computer win points: ${computerWinPoints}`;
     }
+
+    if(CheckForWinner()){
+        FindWinner();
+    }
+}
+
+function CheckForWinner(){
+    if(playerWinPoints >= 5 || computerWinPoints >= 5)
+        return true;
+    return false;
 }
 
 function FindWinner() {
     if (playerWinPoints > computerWinPoints) {
-        console.log(" | | Player has won a game!");
+        gameWinner.textContent = "Player has won a game!";
     }
     else if (playerWinPoints === computerWinPoints) {
-        console.log(" | | Game has ended with a tie!");
+        gameWinner.textContent = "Game has ended with a tie!";
     }
     else {
-        console.log(" | | Computer has won a game!");
+        gameWinner.textContent = "Computer has won a game!";
     }
 }
 
